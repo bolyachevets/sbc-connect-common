81643d5be44b547f411ddcbebc1fbf73b744ec93
deetz99 <dietrich@wolpert.ca>
2024-12-16 10:59:18 -0800

    update main layout file

--- nuxt/layers/core/app/layouts/default.vue
+++ nuxt/layers/core/app/layouts/default.vue
@@ -1,15 +1,9 @@
 <template>
-  <div
-    data-testid="connect-default-layout"
-    class="mx-auto flex min-h-screen w-full flex-col bg-bcGovColor-gray1"
-  >
+  <div class="app-container" data-testid="connect-default-layout">
     <ConnectHeader />
     <ConnectSystemBanner />
     <ConnectBreadcrumb />
-    <main
-      class="mx-auto w-full max-w-bcGovLg grow"
-      data-testid="connect-default-layout-slot"
-    >
+    <main class="app-inner-container app-body">
       <slot />
     </main>
     <ConnectFooter />
