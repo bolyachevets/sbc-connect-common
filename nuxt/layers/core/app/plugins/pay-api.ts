f8abc51f1d0cb8983f758477432fb3b3b3558856
deetz99 <dietrich@wolpert.ca>
2024-12-16 16:41:31 -0800

    fix plugins

--- nuxt/layers/core/app/plugins/pay-api.ts
+++ nuxt/layers/core/app/plugins/pay-api.ts
@@ -1,37 +1,31 @@
-export default defineNuxtPlugin({
-  name: 'core-pay-api-plugin',
-  parallel: true,
-  dependsOn: ['core-keycloak-plugin'],
-  enforce: 'post',
-  setup () {
-    const payApiUrl = useRuntimeConfig().public.payApiURL
-    const errorRedirectPath = useAppConfig().connect.core.plugin.payApi.errorRedirect[401]
-    const { $keycloak } = useNuxtApp()
-    const localePath = useLocalePath()
+export default defineNuxtPlugin(() => {
+  const payApiUrl = useRuntimeConfig().public.payApiURL
+  const errorRedirectPath = useAppConfig().connect.core.plugin.payApi.errorRedirect[401]
+  const { $keycloak } = useNuxtApp()
+  const localePath = useLocalePath()
 
-    const api = $fetch.create({
-      baseURL: payApiUrl,
-      onRequest ({ options }) {
-        const headers = options.headers ||= {}
-        if (Array.isArray(headers)) {
-          headers.push(['Authorization', `Bearer ${$keycloak.token}`])
-        } else if (headers instanceof Headers) {
-          headers.set('Authorization', `Bearer ${$keycloak.token}`)
-        } else {
-          headers.Authorization = `Bearer ${$keycloak.token}`
-        }
-      },
-      async onResponseError ({ response }) {
-        if (response.status === 401) {
-          await navigateTo(localePath(errorRedirectPath))
-        }
+  const api = $fetch.create({
+    baseURL: payApiUrl,
+    onRequest ({ options }) {
+      const headers = options.headers ||= {}
+      if (Array.isArray(headers)) {
+        headers.push(['Authorization', `Bearer ${$keycloak.token}`])
+      } else if (headers instanceof Headers) {
+        headers.set('Authorization', `Bearer ${$keycloak.token}`)
+      } else {
+        headers.Authorization = `Bearer ${$keycloak.token}`
       }
-    })
+    },
+    async onResponseError ({ response }) {
+      if (response.status === 401) {
+        await navigateTo(localePath(errorRedirectPath))
+      }
+    }
+  })
 
-    return {
-      provide: {
-        payApi: api
-      }
+  return {
+    provide: {
+      payApi: api
     }
   }
 })
